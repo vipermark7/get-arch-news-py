85fddd06583a8a3f138286486dc721c668a289b8
Shaffan <shaffan1996@gmail.com>
2023-05-01 19:38:50 -0400

    refactoring + removing html tags in output

--- get-news.py
+++ get-news.py
@@ -1,20 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 from xml.sax.saxutils import unescape
+import re
 
-arch_news_url = "https://archlinux.org/feeds/news/"
-news = requests.get(arch_news_url)
-soup = BeautifulSoup(news.text, 'xml')
-for item in soup.find_all("item")[0:3]:
-    print()
-    print(item.pubDate)
-    print(item.title.text)
-    print(unescape(item.description.text))
+ARCH_NEWS_URL = "https://archlinux.org/feeds/news/"
 
 
+def get_news():
+    news = requests.get(ARCH_NEWS_URL)
+    return news
 
 
+def parse_news_xml():
+    return BeautifulSoup(get_news().text, 'xml')
 
 
+def remove_html_tags(string):
+    return re.sub("<[^<]+?>", "", string)
 
+
+def print_news():
+    soup = parse_news_xml()
+    for item in soup.find_all("item")[0:3]:
+        print()
+        print(remove_html_tags(item.pubDate.text))
+        print(remove_html_tags(item.title.text))
+        print(remove_html_tags(unescape(item.description.text)))
+
+
+parse_news_xml()
+print_news()
 
