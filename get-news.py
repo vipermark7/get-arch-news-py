438be200b327d1ced1b4450d38fea1d94c0e9971
Shaffan <shaffan1996@gmail.com>
2023-04-30 21:57:02 -0400

    add publish date

--- get-news.py
+++ get-news.py
@@ -6,7 +6,9 @@ arch_news_url = "https://archlinux.org/feeds/news/"
 news = requests.get(arch_news_url)
 soup = BeautifulSoup(news.text, 'xml')
 for item in soup.find_all("item")[0:3]:
-    print(item.title)
+    print()
+    print(item.pubDate)
+    print(item.title.text)
     print(unescape(item.description.text))
 
 
