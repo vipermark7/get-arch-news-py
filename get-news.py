83c9faf1bd13ec561c1a6cc47422e2711fcee144
Shaffan <shaffan1996@gmail.com>
2023-04-30 21:47:21 -0400

    fixed an issue with character esacpes

--- get-news.py
+++ get-news.py
@@ -5,8 +5,9 @@ from xml.sax.saxutils import unescape
 arch_news_url = "https://archlinux.org/feeds/news/"
 news = requests.get(arch_news_url)
 soup = BeautifulSoup(news.text, 'xml')
-for description in soup.find_all("description"):
-    print(unescape(description.text))
+for item in soup.find_all("item")[0:3]:
+    print(item.title)
+    print(unescape(item.description.text))
 
 
 
